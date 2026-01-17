ede1b04607000c3f56a8a99f36f2b5f73bf622f9
Toan Tran <toantim.developer@gmail.com>
2026-01-17 12:52:54 +0200

    FILE REFRACTOR: All train, inference, model will be move to model folder inside module recognizers

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+"""
+Hand Gesture Control - MLOps-ready gesture recognition system.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Expert Python Engineer & MLOps Specialist"
+
+# Package level imports for convenience (lazy loading to avoid import errors)
+def __getattr__(name):
+    """Lazy import of submodules."""
+    if name == "GestureClassificationMetrics":
+        from recognizers.utils.metrics import GestureClassificationMetrics
+        return GestureClassificationMetrics
+    elif name == "load_hagrid_samples":
+        from recognizers.data.loaders import load_hagrid_samples
+        return load_hagrid_samples
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+__all__ = [
+    "GestureClassificationMetrics",
+    "load_hagrid_samples",
+]
